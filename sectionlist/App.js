17a1148f3abdd76b929a9d79bc816f8d8869e4c8
Livz123 <57960048+Livz123@users.noreply.github.com>
2025-04-09 11:47:51 -0300

    Update App.js

--- sectionlist/App.js
+++ sectionlist/App.js
@@ -1,10 +1,15 @@
+// Importa o React e o componente SafeAreaView da biblioteca React Native
 import React from "react";
 import { SafeAreaView } from "react-native";
+
+// Importa o componente SectionListComponent de um caminho relativo
 import SectionListComponent from "/components/SectionList.jsx";
 
+// Componente principal do app
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      {/* Renderiza o componente SectionListComponent dentro da área segura */}
       <SectionListComponent />
     </SafeAreaView>
   );
